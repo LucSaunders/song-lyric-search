559a8e1b6b7c92ba7374233b4e8df6a90f9509f1
Luc Saunders <lucsaunders@gmail.com>
2019-06-26 02:49:22 -0400

    Install dependencies. Add navbar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
